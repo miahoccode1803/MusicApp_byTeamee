bf0a53753b0418bd3fe7e7136f76e3633446b8ca
miahoccode1803 <meomeotra993@gmail.com>
2025-01-14 13:38:03 +0700

    myfirst version

---
+++ .dart_tool/flutter_build/1c44af0d2bf6a31af6b25a8b7630b87f/native_assets.d
@@ -0,0 +1,1 @@
+ F:\\musicapp_byme\\.dart_tool\\flutter_build\\1c44af0d2bf6a31af6b25a8b7630b87f\\native_assets.yaml: 
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\musicapp_byme" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\flutter"
+  "PROJECT_DIR=F:\\musicapp_byme"
+  "FLUTTER_ROOT=F:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\musicapp_byme\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\musicapp_byme"
+  "FLUTTER_TARGET=F:\\musicapp_byme\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\musicapp_byme\\.dart_tool\\package_config.json"
+)
